9235a5ed02686cfcd6b508a4ace9f9aff02f09da
DeathMonster666 <alex6200@gmail.com>
2015-01-11 05:50:26 -0500

    Refactoring lstm code

--- lib/LSTMLayer.py
+++ lib/LSTMLayer.py
@@ -3,6 +3,7 @@ import theano.tensor as T
 import numpy
 import numpy.random as rng
 import math
+import numpy as np
 
 from ConsiderConstant import consider_constant
 
@@ -27,11 +28,11 @@ from ConsiderConstant import consider_constant
 
 class LSTMLayer: 
 
-    def __init__(self, inputSize, controllerSize, memorySize, outputSize, initialScale, useReluReadGate = True):
+    def __init__(self, inputSize, controllerSize, memorySize, outputSize, initialScale, useReluReadGate = True, params = None):
 
         readGateSize = memorySize
         readDeltaSize = memorySize
-        writeGateSize = memorySize
+        writeGateSize = outputSize
         keepGateSize = memorySize
 
         scale = initialScale
@@ -51,82 +52,84 @@ class LSTMLayer:
         else:
             readGateScale = initialScale
 
-        self.W_controller_0 = theano.shared(numpy.asarray(scale * rng.normal(size = (inputSize + 1 * memorySize, controllerSize)), dtype = theano.config.floatX), name = "Controller weights 0")
+        if params == None: 
+            self.params = {}
 
-        self.W_controller_1 = theano.shared(numpy.asarray(scale * rng.normal(size = (controllerSize, controllerSize)), dtype = theano.config.floatX), name = "Controller weights 1")
-        self.W_controller = theano.shared(numpy.asarray(scale * rng.normal(size = (controllerSize, controllerSize)), dtype = theano.config.floatX), name = "Controller weights 2")
+            self.params["W_controller_0"] = theano.shared(numpy.asarray(scale * rng.normal(size = (inputSize, controllerSize)), dtype = theano.config.floatX), name = "Controller weights 0")
 
-        self.W_readgate = theano.shared(numpy.asarray(readGateScale * rng.normal(size = (readGateSize, inputSize + 1 * controllerSize)), dtype = theano.config.floatX), name = "read gate weights")
+            self.params["W_controller_1"] = theano.shared(numpy.asarray(scale * rng.normal(size = (controllerSize, controllerSize)), dtype = theano.config.floatX), name = "Controller weights 1")
+            self.params["W_controller"] = theano.shared(numpy.asarray(scale * rng.normal(size = (controllerSize, controllerSize)), dtype = theano.config.floatX), name = "Controller weights 2")
 
-        self.W_readdelta = theano.shared(numpy.asarray(scale * rng.normal(size = (readDeltaSize, inputSize + 1 * controllerSize)), dtype = theano.config.floatX), name = "readdelta weights")
+            self.params["W_readgate"] = theano.shared(numpy.asarray(readGateScale * rng.normal(size = (readGateSize, inputSize + 1 * controllerSize)), dtype = theano.config.floatX), name = "read gate weights")
 
-        self.W_writegate = theano.shared(numpy.asarray(scale * rng.normal(size = (writeGateSize, inputSize + 1 * memorySize)), dtype = theano.config.floatX), name = "writegate weights")
+            self.params["W_readdelta"] = theano.shared(numpy.asarray(scale * rng.normal(size = (readDeltaSize, inputSize + 1 * controllerSize)), dtype = theano.config.floatX), name = "readdelta weights")
 
-        self.W_keepgate = theano.shared(numpy.asarray(scale * rng.normal(size = (keepGateSize, inputSize + 1 * memorySize)), dtype = theano.config.floatX), name = "keepgate weights")
+            self.params["W_writegate"] = theano.shared(numpy.asarray(scale * rng.normal(size = (writeGateSize, inputSize + 1 * controllerSize)), dtype = theano.config.floatX), name = "writegate weights")
 
-        self.W_output = theano.shared(numpy.asarray(reluScale * rng.normal(size = (outputSize, inputSize + memorySize + controllerSize)), dtype = theano.config.floatX), name = "output weights")
+            self.params["W_keepgate"] = theano.shared(numpy.asarray(scale * rng.normal(size = (keepGateSize, inputSize + 1 * controllerSize)), dtype = theano.config.floatX), name = "keepgate weights")
 
-        self.b_controller_0 = theano.shared(numpy.asarray(numpy.zeros(shape = controllerSize), dtype = theano.config.floatX), name = "b_controller_0")
-        self.b_controller_1 = theano.shared(numpy.asarray(numpy.zeros(shape = controllerSize), dtype = theano.config.floatX), name = "b_controller_1")
-        self.b_controller = theano.shared(numpy.asarray(numpy.zeros(shape = controllerSize), dtype = theano.config.floatX), name = "b_controller")
+            self.params["W_output"] = theano.shared(numpy.asarray(reluScale * rng.normal(size = (outputSize, inputSize + memorySize + controllerSize)), dtype = theano.config.floatX), name = "output weights")
 
-        self.b_readgate = theano.shared(numpy.asarray(numpy.zeros(shape = readGateSize), dtype = theano.config.floatX), name = "b_readgate")
+            self.params["b_controller_0"] = theano.shared(numpy.asarray(numpy.zeros(shape = controllerSize), dtype = theano.config.floatX), name = "b_controller_0")
+            self.params["b_controller_1"] = theano.shared(numpy.asarray(numpy.zeros(shape = controllerSize), dtype = theano.config.floatX), name = "b_controller_1")
+            self.params["b_controller"] = theano.shared(numpy.asarray(numpy.zeros(shape = controllerSize), dtype = theano.config.floatX), name = "b_controller")
 
-        self.b_readdelta = theano.shared(numpy.asarray(numpy.zeros(shape = readDeltaSize), dtype = theano.config.floatX), name = "b_readdelta")
+            self.params["b_readgate"] = theano.shared(numpy.asarray(numpy.zeros(shape = readGateSize), dtype = theano.config.floatX), name = "b_readgate")
 
-        self.b_writegate = theano.shared(numpy.asarray(writeGateInitialBias + numpy.zeros(shape = writeGateSize), dtype = theano.config.floatX), name = "b_writegate")
+            self.params["b_readdelta"] = theano.shared(numpy.asarray(numpy.zeros(shape = readDeltaSize), dtype = theano.config.floatX), name = "b_readdelta")
 
-        self.b_keepgate = theano.shared(numpy.asarray(numpy.zeros(shape = keepGateSize) + keepGateInitialBias, dtype = theano.config.floatX), name = "b_keepgate")
+            self.params["b_writegate"] = theano.shared(numpy.asarray(writeGateInitialBias + numpy.zeros(shape = writeGateSize), dtype = theano.config.floatX), name = "b_writegate")
 
-        self.b_output = theano.shared(numpy.asarray(numpy.zeros(shape = outputSize), dtype = theano.config.floatX), name = "b_output")
+            self.params["b_keepgate"] = theano.shared(numpy.asarray(numpy.zeros(shape = keepGateSize) + keepGateInitialBias, dtype = theano.config.floatX), name = "b_keepgate")
 
-        paramLst = [self.W_controller, self.W_controller_0, self.W_controller_1, self.b_controller_1, self.b_controller_0, self.W_readgate, self.W_readdelta, self.W_writegate, self.W_keepgate, self.W_output, self.b_controller, self.b_readgate, self.b_readdelta, self.b_writegate, self.b_keepgate, self.b_output]
-
-        self.params = {}
-
-        for param in paramLst: 
-            self.params[param.name] = param
-        
+            self.params["b_output"] = theano.shared(numpy.asarray(numpy.zeros(shape = outputSize), dtype = theano.config.floatX), name = "b_output")
+        else: 
+            self.params = params
 
     '''
         Returns new controller, new memory
     '''
-    def getOutputs(self, previousController, previousMemory, input_layer): 
+    def getOutputs(self, previousMemory, input_layer): 
 
-        assert(previousController.ndim == previousMemory.ndim)
+        print "prev memory dim", previousMemory.ndim
+        print "input layer dim", input_layer.ndim
         assert(previousMemory.ndim == input_layer.ndim)
 
-        if previousController.ndim == 1: 
+        if previousMemory.ndim == 1: 
             axisConcat = 0
         else:
             axisConcat = 1
 
-        controller_0 = T.maximum(0.0, T.dot(T.concatenate([consider_constant(previousMemory), 1.0 * input_layer], axis = axisConcat), self.W_controller_0) + self.b_controller_0)        
+        controller_0 = T.maximum(0.0, T.dot(input_layer, self.params["W_controller_0"]) + self.params["b_controller_0"])    
 
-        controller_1 = T.maximum(0.0, T.dot(controller_0, self.W_controller_1) + self.b_controller_1)
+        controller_1 = T.maximum(0.0, T.dot(controller_0, self.params["W_controller_1"]) + self.params["b_controller_1"])
 
-        controller = T.maximum(0.0, T.dot(controller_1, self.W_controller) + self.b_controller)
+        controller = T.maximum(0.0, T.dot(controller_1, self.params["W_controller"]) + self.params["b_controller"])
 
         #Have multiple layers in controller?  This determines what gets passed in / out from the network.  
 
         if self.useReluReadGate: 
-            readgate = T.maximum(0.0, (T.dot(T.concatenate([controller, input_layer], axis = axisConcat), self.W_readgate.T) + self.b_readgate))
+            readgate = T.maximum(0.0, (T.dot(T.concatenate([controller, input_layer], axis = axisConcat), self.params["W_readgate"].T) + self.params["b_readgate"]))
         else:
-            readgate = T.nnet.sigmoid(T.dot(T.concatenate([controller, input_layer], axis = axisConcat), self.W_readgate.T) + self.b_readgate)
+            readgate = T.nnet.sigmoid(T.dot(T.concatenate([controller, input_layer], axis = axisConcat), self.params["W_readgate"].T) + self.params["b_readgate"])
 
-        readdelta = T.tanh(T.dot(T.concatenate([controller, input_layer], axis = axisConcat), self.W_readdelta.T) + self.b_readdelta)
+        readdelta = T.tanh(T.dot(T.concatenate([controller, input_layer], axis = axisConcat), self.params["W_readdelta"].T) + self.params["b_readdelta"])
 
-        keepgate = T.nnet.sigmoid(T.dot(T.concatenate([controller, input_layer], axis = axisConcat), self.W_keepgate.T) + self.b_keepgate)
+        keepgate = T.nnet.sigmoid(T.dot(T.concatenate([controller, input_layer], axis = axisConcat), self.params["W_keepgate"].T) + self.params["b_keepgate"])
 
-        memory_intermediate = previousMemory * keepgate + readgate * readdelta
+        memory = previousMemory * keepgate + readgate * readdelta
 
-        writegate = T.nnet.sigmoid(T.dot(T.concatenate([controller, input_layer], axis = axisConcat), self.W_writegate.T) + self.b_writegate)
+        writegate = T.nnet.sigmoid(T.dot(T.concatenate([controller, input_layer], axis = axisConcat), self.params["W_writegate"].T) + self.params["b_writegate"])
 
-        memory = memory_intermediate
+        output = writegate * T.maximum(0.0, T.dot(T.concatenate([controller, memory, input_layer], axis = axisConcat), self.params["W_output"].T) + self.params["b_output"])
 
-        output = writegate * T.maximum(0.0, T.dot(T.concatenate([controller, memory, input_layer], axis = axisConcat), self.W_output.T) + self.b_output)
+        return memory, output
 
-        return controller, memory_intermediate, output
+        #Only works if output size is the same as the size of the memory.  This either means that output is getting size of memory or memory is getting size of output
+
+
+
+
 
 
 
--- lib/loopRnn.py
+++ lib/loopRnn.py
@@ -14,121 +14,161 @@ Build discriminator, generator.  Create sample sequences from multivariate norma
 
 '''
 
+cast32 = lambda x: T.cast(x, 'float32')
+
+def weight_matrix(shape): 
+    random_seed = 1235
+    rng = np.random.RandomState(random_seed)
+    scale = 0.05
+    return theano.shared(numpy.asarray(scale * rng.normal(size = shape), dtype = theano.config.floatX))
+
 #Provide two methods.  One gets a network that takes an X sequence and outputs p(X).  
-def discriminator(X): 
+def discriminator(X, sequenceLength, batch_size, params = None): 
 
-    lstm = LSTMLayer.LSTMLayer(inputSize = 1, controllerSize = 500, memorySize = 500, outputSize = 500, initialScale = 0.05)
+    memorySize = 200
+    outputSize = 200
 
-    h1 = T.matrix()
-    memory_0 = T.matrix()
-    output_0 = T.matrix()
+    lstm = LSTMLayer.LSTMLayer(inputSize = 1, controllerSize = 500, memorySize = memorySize, outputSize = outputSize, initialScale = 0.05, params = params)
 
-    def oneStep(prevMemory, prevOutput, inputSequence):
+    memory_0 = theano.shared(np.zeros(shape = (batch_size, memorySize), dtype = 'float32'))
+    output_0 = theano.shared(np.zeros(shape = (batch_size, outputSize), dtype = 'float32'))
+
+    def oneStep(inputSequence, prevMemory, prevOutput):
         memory, output = lstm.getOutputs(prevMemory, inputSequence)
-        return memory, output
+        return cast32(memory), cast32(output)
 
-    output_sequence, _ = theano.scan(oneStep, sequences = [X], outputs_info = [memory_0, output_0], n_steps = sequenceLength)
+    output_vars, _ = theano.scan(oneStep, sequences = [X], outputs_info = [memory_0, output_0], n_steps = sequenceLength)
 
-    params = lstm.params
+    final_memory, final_output = output_vars
 
-    params["W_out_px"] = weight_matrix((500, 1))
-    params["b_out_px"] = weight_matrix((1,))
+    if params == None: 
+        params = lstm.params
+        params["W_out_px"] = weight_matrix((outputSize, 1))
+        params["b_out_px"] = weight_matrix((1,))
 
-    p_X = T.nnet.sigmoid(T.dot(output_sequence[-1], params["W_out_px"]) + params["b_out_px"])
+    p_X = T.nnet.sigmoid(T.dot(final_output[-1], params["W_out_px"]) + params["b_out_px"])
 
     return params, p_X
 
+def generate_noise(size, batch_size): 
+    random_seed = 1234
+    rng = np.random.RandomState(random_seed)
+    srng = theano.tensor.shared_randomstreams.RandomStreams(rng.randint(999999))
+
+    num_comp = 5
+    comp_size = size / num_comp
+
+    z1 = srng.normal(size = (batch_size, comp_size))
+    z2 = srng.binomial(size = (batch_size,comp_size), n = 1, p = 0.5, dtype = 'float32')
+    z3 = srng.multinomial(size = (batch_size,), n = 1, pvals = [1.0 / (comp_size)] * comp_size, dtype = 'float32')
+    z4 = srng.multinomial(size = (batch_size,), n = 1, pvals = [1.0 / (comp_size)] * comp_size, dtype = 'float32')
+    z5 = srng.multinomial(size = (batch_size,), n = 1, pvals = [1.0 / (comp_size)] * (comp_size), dtype = 'float32')
+
+    z = T.concatenate([z1,z2,z3,z4,z5], axis = 1)
+
+    return z
+
 #Takes a matrix of noise variables, loads these into memory initially (concatenated with blank memory spots).  
 #Outputs an X sequence.  
-def generator(): 
+def generator(sequenceLength, batch_size, params): 
 
-    return params, X
+    size_noise_memory = 50
+    size_other_memory = 350
+
+    z = cast32(generate_noise(size_noise_memory, batch_size))
+
+    other_memory = theano.shared(np.zeros(shape = (batch_size, size_other_memory), dtype = 'float32'), name = "other_memory")    
+
+    initial_memory = T.concatenate([z, other_memory], axis = 1)
+
+    memorySize = size_noise_memory + size_other_memory
+    outputSize = 1
+    inputSize = 1
+
+    output_0 = theano.shared(np.zeros(shape = (batch_size, outputSize), dtype = 'float32'), name = "output_tensor")
+
+    lstm = LSTMLayer.LSTMLayer(inputSize = inputSize, controllerSize = 200, memorySize = memorySize, outputSize = outputSize, initialScale = 0.05, params = params)
+
+    def oneStep(prevMemory, prevOutput):
+        inputSequence = np.zeros(shape = (batch_size, inputSize), dtype = 'float32')
+        memory, output = lstm.getOutputs(prevMemory, inputSequence)
+        return cast32(memory), cast32(output)
+
+    output_vars, _ = theano.scan(oneStep, sequences = [], outputs_info = [initial_memory, output_0], n_steps = sequenceLength)
+
+    final_memory, final_output = output_vars
+
+    params = lstm.params
+
+    return params, final_output
 
 
 sys.setrecursionlimit(100000)
 
 
-if __name__ == "__main__": 
 
-    print "RUN"
 
-    weightSize = 60
+def test_gan_rnn(): 
 
-    lstm = LSTMLayer.LSTMLayer(controllerSize = 60, memorySize = 60, outputSize = 60, inputSize = 60, initialScale = 0.05, useReluReadGate = True)
+    #Set up parameters
 
-    W = theano.shared(numpy.random.normal(size = (weightSize,weightSize)))
+    #The time series for each element of the batch
+    #sequenceLength x batch_size x 1
+    X_observed = T.tensor3()
 
-    print W.shape
-
-    def oneStep(prevH, prevMemory, prevController): 
-
-        print "memory ndim should be 2", prevMemory.ndim
-
-        controller1, memory1, h1 = lstm.getOutputs(prevController, prevMemory, prevH)
-
-        return controller1, memory1, h1
+    #Each sequence is either 1 or 0.  
+    Y = T.vector()
 
     sequenceLength = 20
+    batch_size = 100
 
-    print "Sequence Length", sequenceLength, "Number of Hidden Units:", weightSize
+    #Discriminator on observed sample, discriminator on generated sample
 
-    h0 = T.matrix()
-    memory_0 = T.matrix()
-    controller_0 = T.matrix()
-    h1 = h0
+    params_disc, p_x_observed = discriminator(X_observed, sequenceLength, batch_size, params = None)
+    params_gen, x_gen = generator(sequenceLength, batch_size, params = None)
+    params_disc, p_x_generated = discriminator(x_gen, sequenceLength, batch_size, params = params_disc)
 
-    batch_size = 400
+    loss_generator = adversarial_loss_generator(p_x_generated = p_x_generated)
 
-    new_h = [h0]
-    new_memory = [memory_0]
-    new_controller = [controller_0]
+    loss_discriminator = adversarial_loss_discriminator(p_x_generated = p_x_generated, p_x_observed = p_x_observed)
 
-    new_h_scan, _ = theano.scan(oneStep, sequences = [], outputs_info = [h1, memory_0, controller_0], n_steps = sequenceLength)
+    #Generate loss, updates
 
-    print "output n dim should be 3", np.asarray(new_h_scan[0]).ndim
-    print "new h scan", new_h_scan[0]
+    generate_sample = theano.function(inputs = [], outputs = [x_gen])
 
-    #print "starting grad for loop"
-    #timeStart = time.time()
-    #g = T.grad(sum(map(T.sum, new_h)), lstm.params)
-    #print "time spent on for loop grad", time.time() - timeStart
+    g = generate_sample()
 
-    timeStart = time.time()
-    g_scan = T.grad(T.sum(new_h_scan), lstm.params.values())
-    print "time spent on scan grad", time.time() - timeStart
+    print g[0].shape
 
-    #timeStart = time.time()
-    #f = theano.function(inputs = [h0, memory_0], outputs = [new_h[-1]] + g)
-    #print "time spent compling for loop", time.time() - timeStart
+    sample_prob = theano.function(inputs = [X_observed], outputs = [p_x])
 
-    timeStart = time.time()
-    f_scan = theano.function(inputs = [h0, memory_0, controller_0], outputs = [new_h_scan[0]])
-    print "time spent compiling scan", time.time() - timeStart
-
-    numIter = 20
-
-    #timeStart = time.time()
-    #for i in range(0, numIter): 
-    #    f([[1.0] * weightSize] * batch_size, [[1.0] * weightSize] * batch_size)
-    #print "time", time.time() - timeStart
-
-    timeStart = time.time()
-
-    for i in range(0, numIter): 
-        output = f_scan([[1.0] * weightSize] * batch_size, [[1.0] * weightSize] * batch_size, [[1.0] * weightSize] * batch_size)
-
-        print np.asarray(output[0]).shape
-
-    print "time for scan version", time.time() - timeStart
+    sampled = sample_prob(g[0])
 
 
 
+    #Train in loop
 
+    #Evaluate?
 
+    pass
 
+#Generate samples from two different distributions and see if discriminator can learn to separate them
+def test_discriminator_1(): 
+    #The time series for each element of the batch
+    #sequenceLength x batch_size x 1
+    X = T.tensor3()
+    #Each sequence is either 1 or 0.  
+    Y = T.vector()
 
+    sequenceLength = 20
+    batch_size = 100
 
+    params_disc, p_x = discriminator(X, sequenceLength, batch_size)
 
+    
+
+if __name__ == "__main__": 
+    test_gan_rnn()
 
 
 
